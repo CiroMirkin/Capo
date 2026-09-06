3d755e7fd7a083573cd59a43b9d24b665ee56548
ciromirkin@gmail.com <ciromirkin@gmail.com>
2026-09-06 19:45:32 -0300

    feat(preferences): módulo sidebar con el lado del rail

---
+++ web-app/src/shared/preferences/sidebar/index.ts
@@ -0,0 +1,8 @@
+export {
+	defaultSidebarSide,
+	isValidSidebarSide,
+	sidebarSideLocalStorageKey,
+} from './model/sidebarSide'
+export type { SidebarSide } from './model/sidebarSide'
+export { useSidebarSide } from './hooks/useSidebarSide'
+export { ToggleSidebarSide } from './ui/ToggleSidebarSide'
