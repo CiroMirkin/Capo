6ae3f1f1a0b192557082e308a5f1f7c45595b03c
CiroMirkin <ciromirkin@gmail.com>
2026-09-09 10:46:08 -0300

    feat(usage-history): centra calendario y card de hoy, fija ancho de UsageRecord

--- web-app/src/features/usage-history/ui/UsageHistory.tsx
+++ web-app/src/features/usage-history/ui/UsageHistory.tsx
@@ -3,6 +3,7 @@ import UsageRecord from './UsageRecord'
 import { EmptySpaceText } from '@/shared/ui/atoms/EmptySpaceText'
 import { Spinner } from '@/shared/ui/atoms/spinner'
 import { useTranslation } from 'react-i18next'
+import UsageCalendar from './UsageCalendar'
 
 export default function UsageHistory() {
 	const { t } = useTranslation()
@@ -11,14 +12,20 @@ export default function UsageHistory() {
 	if (isLoading) return <Spinner size={30} />
 
 	const theUsageHistoryIsEmpty = !usageHistory.length
-	const usageRecords = [...usageHistory]
+	const [today, ...usageRecords] = [...usageHistory]
 		.map((usageRecord) => <UsageRecord key={usageRecord.date} usageRecord={usageRecord} />)
 		.reverse()
 
 	return (
-		<div className='flex flex-wrap gap-4'>
-			{theUsageHistoryIsEmpty && <EmptySpaceText>{t('usage_history.empty')}</EmptySpaceText>}
-			{usageRecords}
+		<div className='flex flex-col gap-4'>
+			<div className='flex flex-col md:flex-row md:justify-center gap-4'>
+				<UsageCalendar usageHistory={usageHistory} />
+				{today}
+			</div>
+			<div className='flex flex-wrap gap-4'>
+				{theUsageHistoryIsEmpty && <EmptySpaceText>{t('usage_history.empty')}</EmptySpaceText>}
+				{usageRecords}
+			</div>
 		</div>
 	)
 }
