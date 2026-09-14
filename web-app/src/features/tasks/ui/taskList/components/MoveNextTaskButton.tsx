7f54bbcddc167ccfea5874a019d6410bfa1d777a
ciromirkin@gmail.com <ciromirkin@gmail.com>
2026-09-14 13:33:35 -0300

    refactor(tasks): unifica TaskInBoardActions y elimina MoveButtons

---
+++ web-app/src/features/tasks/ui/taskList/components/MoveNextTaskButton.tsx
@@ -0,0 +1,32 @@
+import { useTranslation } from 'react-i18next'
+import { useCheckIfTaskIsInTheLastColumn } from '@/features/tasks/ui/Columns/hooks/useCheckIfTaskIsInTheLastColumn'
+import { Button } from '@/shared/ui/atoms/button'
+import { useDataOfTheTask } from '../hooks/useDataOfTheTask'
+import { ArrowRightIcon } from '@/shared/ui/atoms/icons'
+import { useMoveTaskToNextColumn } from '../hooks/useMoveTaskToNextColumn'
+
+interface Props {
+	handleClick: (action: () => void) => void
+	className?: string
+}
+
+export function MoveNextTaskButton({ handleClick, className }: Props) {
+	const { t } = useTranslation()
+	const data = useDataOfTheTask()
+	const isTheTaskInTheLastColumn = useCheckIfTaskIsInTheLastColumn(data)
+	const moveTaskToNextColumn = useMoveTaskToNextColumn()
+
+	return (
+		<Button
+			size='sm'
+			disabled={isTheTaskInTheLastColumn}
+			variant='ghost'
+			className={className}
+			data-testid='BotonParaAvanzarTarea'
+			onClick={() => handleClick(() => moveTaskToNextColumn(data))}
+			title={t('task_buttons.next_btn')}
+		>
+			<ArrowRightIcon />
+		</Button>
+	)
+}
