34870fec942e2c4ab6fc283e391bbafd3eeedffd
CiroMirkin <ciromirkin@gmail.com>
2026-09-21 00:27:26 -0300

    feat: separa "/", "/home", "/guest" y "/dashboard" segun sesion

--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -1,7 +1,15 @@
 'use client'
 
-import UserDashboard from './UserDashboard'
+import { useSession } from '@/features/auth'
+import { redirect } from 'next/navigation'
+import { Home } from './home/Home'
 
-export default function HomePage() {
-	return <UserDashboard />
+export default function RootPage() {
+	const { session, isLoading } = useSession()
+
+	if (!isLoading && session) {
+		redirect('/dashboard')
+	}
+
+	return <Home />
 }
